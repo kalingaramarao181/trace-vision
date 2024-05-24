ff89fe620ca0b85f04601cd6bbaa1c78d488014a
kalingaramarao181 <122093483+kalingaramarao181@users.noreply.github.com>
2024-05-25 01:46:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
 
 // Add CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Origin', 'https://tracevision.beedatatech.com/'); // Replace with your frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
